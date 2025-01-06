a2246a48f9c120e356671b8af52e6003f7d7d307
Torsten Keßler <t.kessler@posteo.de>
2025-01-06 18:58:14 +0100

    Add highlighting for terratest

---
+++ queries/highlights.scm
@@ -0,0 +1,24 @@
+; inherits terra
+
+"test" @keyword
+
+(testset_statement
+[
+  "testset"
+  "do"
+  "end"
+] @keyword)
+
+(testenv_statement
+[
+  "testenv"
+  "do"
+  "end"
+] @keyword)
+
+(terracode_statement
+[
+  "terracode"
+  "end"
+] @keyword)
+
